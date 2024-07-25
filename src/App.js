0cb7c499a35c090d456dc68be3f71016ad78c8fe
admin <qkdtmdqo14@gmail.com>
2024-07-25 15:12:47 +0900

    # 2. 선언 및 props 사용

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import React from 'react';
-import Hello from './components/Hello';
-import Bye from './components/Bye';
-import Greet from './components/Greet';
+import ExpenseItem from './components/Expenses/ExpenseItem';
 
 
-function App() {
+const App = () => {
+
   return (
     <>
-    {/* export한 Hello 태그 렌더링 시킴 */}
-       <Hello /> 
-       <Bye />
-       <Greet />
+     <ExpenseItem title="하하호호" price={4900}/>
+     <ExpenseItem title="낄낄깔깔" price={8000}/>
+     <ExpenseItem title="꾸꾸까까" price={6800}/>
     </>
   );
 }
