cb0f16d22279d3e0723168b8ca84b4e96a270331
admin <qkdtmdqo14@gmail.com>
2024-07-25 15:38:48 +0900

    # 3. react props 2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,28 @@ import ExpenseItem from './components/Expenses/ExpenseItem';
 
 const App = () => {
 
+  // 서버에서 지출항목 JSON 배열을 응답 받음
+  const expenses = [{
+    title: '수영장 등록',
+    price: 100000,
+    date: new Date(2024, 6 - 1, 3)
+  },
+  { title: '다이소',
+    price: 8000,
+    date: new Date(2024, 7 - 1, 14)
+  },
+  {
+    title: '김밥',
+    price: 4900,
+    date: new Date(2024, 7 - 1, 25)
+  }
+]
+
   return (
     <>
-     <ExpenseItem title="하하호호" price={4900}/>
-     <ExpenseItem title="낄낄깔깔" price={8000}/>
-     <ExpenseItem title="꾸꾸까까" price={6800}/>
+     <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date} />
+     <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date} />
+     <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date} />
     </>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
-export const ExpenseItem = (aaa) => {
+export const ExpenseItem = ({date, title, price: exPrice}) => {  // 디스턱처링하여 앞에 붙이지 않고 꺼내서 단순하게 사용 !
+    // console.log('props: ', aaa);
 
-    console.log('props: ', aaa);
     //변수 선언
-    const expenseDate = new Date(2024, 7, 25);
-    const expenseTitle = aaa.title;  // props 파라미터 객체값을 가져와서 사용 !
-    const expensePrice = aaa.price;
+    const expenseDate = date;
+    const expenseTitle = title;  // props 파라미터 객체값을 가져와서 사용 !
+    const expensePrice = exPrice;
 
     // 함수 선언
 
@@ -17,11 +18,11 @@ export const ExpenseItem = (aaa) => {
     };
 
     const makeFormattedDate = () => {
-        const year = expenseDate.getFullYear();
-        const month = expenseDate.getMonth();
-        const date = expenseDate.getDate();
+        const year = date.getFullYear();
+        const month = date.getMonth();
+        const day = date.getDate();
 
-        return `${year}년 ${month}월 ${make2digit(date)}일`;
+        return `${year}년 ${month}월 ${make2digit(day)}일`;
     };
 
     // 원화 표기법으로 변환
@@ -29,9 +30,10 @@ export const ExpenseItem = (aaa) => {
 
   return (
     <div className='expense-item'>
-        <div>{makeFormattedDate()}</div>
+        {/* 날짜처리하는 컴포넌트 별도로 처리 ! */}
+        <ExpenseDate exDate={date}/>
         <div className='expense-item__description'>
-            <h2>{expenseTitle}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>{formattedPrice}원</div>
         </div>
     </div>
