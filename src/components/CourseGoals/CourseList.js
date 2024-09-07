a5549bf1e5081e49a0924c2222eb9bc20e788933
goodzeee <qkdtmdqo14@gmail.com>
2024-09-07 15:56:34 +0900

    # 목표 설정 더미 데이터 items로 보낸거 가져와 List>item 구조로

--- src/components/CourseGoals/CourseList.js
+++ src/components/CourseGoals/CourseList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './CourseList.css';
+import CourseItem from './CourseItem';
 
 const CourseList = ({items}) => {
   return (
     <ul className='goal-list'>
         {
             // 더미 데이터 items 가져와서 목표 텍스트 반복하여 리스트 채우기
-            items.map(item => <li>{item.text}</li>)
+            items.map(item => <CourseItem key={item.id} item={item}/>)
         }
     </ul>
   );
