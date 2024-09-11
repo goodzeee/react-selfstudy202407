d186da26a56167f06d64e048cd39e73a5a186f88
goodzeee <qkdtmdqo14@gmail.com>
2024-09-11 13:16:34 +0900

    # Portal - {ReactDOM.createPortal(태워 보낼 태그)}

--- src/components/UI/Modal/ErrorModal.js
+++ src/components/UI/Modal/ErrorModal.js
@@ -7,37 +7,54 @@ import Button from '../Button';
 import styles from './ErrorModal.module.css';
 // import Portal from '../Portal/Portal';
 
-// const BackDrop = ({ onClose }) => {
-//   return (
-//     <div
-//       className={styles.backdrop}
-//       onClick={onClose}
-//     ></div>
-//   );
-// };
+// return문에 있는 백드롭 태그 추출하기
+const BackDrop = ({ onClose }) => {
+  return (
+    <div className={styles.backdrop} onClick={onClose}></div>
+  );
+};
 
-// const ModalOverlay = ({ title, message, onClose }) => {
-//   return (
-//     <Card className={styles.modal}>
-//       <header className={styles.header}>
-//         <h2>{title}</h2>
-//       </header>
-//       <div className={styles.content}>
-//         <p>{message}</p>
-//       </div>
-//       <footer className={styles.actions}>
-//         <Button onClick={onClose}>Okay</Button>
-//       </footer>
-//     </Card>
-//   );
-// };
+// return문에 백드롭 밑에 오버레이 부분 태그 추출하기
+const ModalOverlay = ({ title, message, onClose }) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 // 에러 모달창 - 제목, 내용, 닫는 이벤트
 const ErrorModal = ({ title, message, onClose }) => {
   return (
     <>
-    <div className={styles.backdrop} onClick={onClose}></div>
-        <Card className={styles.modal}>
+       {
+        ReactDOM.createPortal(
+          <BackDrop onClose={onClose} />,
+          // 백드롭을 index.html에 만든 'backdrop-root' 아이디에 태워 보낸다 (포탈)
+          document.getElementById('backdrop-root')
+        )
+      }
+      {
+        ReactDOM.createPortal(
+          <ModalOverlay title={title} message={message} onClose={onClose}/>,
+          document.getElementById('overlay-root')
+        )
+      }
+
+    {/* ❕ 리팩토링 : 한 파일에서 만든 태그 가져와서 특정 위치에 return 할 수 있음 */}
+    {/* <BackDrop onClose={onClose} />
+    <ModalOverlay title={title} message={message} onClose={onClose}/> */}
+    
+    {/* <div className={styles.backdrop} onClick={onClose}></div> */}
+        {/* <Card className={styles.modal}>
             <header className={styles.header}> 
                 <h2> {title}
                 </h2>
@@ -48,18 +65,7 @@ const ErrorModal = ({ title, message, onClose }) => {
                     <footer className={styles.actions}>
                         <Button onClick={onClose}>Okay</Button>
                     </footer>
-        </Card>
-      {/* <Portal destId="backdrop-root">
-        <BackDrop onClose={onClose} />
-      </Portal>
-
-      <Portal destId="overlay-root">
-        <ModalOverlay
-          title={title}
-          message={message}
-          onClose={onClose}
-        />
-      </Portal> */}
+        </Card> */}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
+// index.html > index.js > App.js 구조 흐름 !
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
