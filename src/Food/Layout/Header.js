6e86852afc8556caa82fe54c9df58134d640bc4d
goodzeee <qkdtmdqo14@gmail.com>
2024-09-15 14:30:44 +0900

    # Cart 장바구니 실습 초기 세팅

---
+++ src/Food/Layout/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import Header from './components/Food/Layout/Header';
+import styles from './Header.module.scss'
+
+const Header = () => {
+  return (
+    <>
+    <header className={styles.header}>
+        <h1>React Meals</h1>
+    </header>
+    <div>
+        <img />
+    </div>
+    </>
+  )
+}
+
+export default Header
