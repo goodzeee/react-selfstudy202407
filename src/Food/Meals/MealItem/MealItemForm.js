712994d311fd338f19451570946b9c2fa7ad5c40
goodzeee <qkdtmdqo14@gmail.com>
2024-09-17 22:58:24 +0900

    # Input 컴포넌트 속성값 스프레드

--- src/Food/Meals/MealItem/MealItemForm.js
+++ src/Food/Meals/MealItem/MealItemForm.js
@@ -9,14 +9,14 @@ const MealItemForm = ( props ) => {
       <Input
         // ref={inputRef}
         label='수량'
-        // input={{
-        //   id: 'amount_' + props.id,
-        //   type: 'number',
-        //   min: '1',
-        //   max: '5',
-        //   step: '1',
-        //   defaultValue: '1',
-        // }}
+        inputAttr={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
       /> 
       <button>담기</button>
     </form>
