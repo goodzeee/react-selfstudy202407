497c60057eb258e27ee3f4f161f649d60373045e
goodzeee <qkdtmdqo14@gmail.com>
2024-09-20 19:50:50 +0900

    # 장바구니에 추가할 상품 갯수 -  forwardRef 보내고 가져와 사용하는 방법

--- src/Food/Meals/MealItem/MealItemForm.js
+++ src/Food/Meals/MealItem/MealItemForm.js
@@ -4,10 +4,25 @@ import Input from '../../../components/UI/Input/Input';
 
 const MealItemForm = ( props ) => {
 
+  // 선택한 수량 값
+  const [amount, setAmount] = useState(0);
+
+  // 선택한 수량 값 가져오기
+  const inputRef = useRef();
+
+  const submitHandler = e => {
+    e.preventDefault();
+    const selectedValue = inputRef.current.value;
+    console.log('select: ', selectedValue);
+    
+  };
+
   return (
-    <form className={styles.form} >
+    <form className={styles.form} onSubmit={submitHandler}>
+      {/* <input ref={inputRef} type='number' /> */}
+      {/* ref는 props로 전달이 안된다 !! */}
       <Input
-        // ref={inputRef}
+        ref={inputRef}
         label='수량'
         inputAttr={{
           id: 'amount_' + props.id,
